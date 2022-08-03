0c59193dc74e54600f4d5a9a96efecb35d0e0b0b
Teimuraz Kozanashvili <kozanashvili.teimuraz@gmail.com>
2022-08-03 15:20:30 +0300

    Comment deprecated option from Mongoose connect initialization

--- back/config/db.js
+++ back/config/db.js
@@ -5,7 +5,10 @@ const connectDB = async () => {
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
-			useCreateIndex: true
+			/// Option `useCreateIndex` was deprecated in Mongoose 6
+			/// More info: https://www.mongodb.com/community/forums/t/option-usecreateindex-is-not-supported/123048
+			
+			// useCreateIndex: true
 		})
 
 		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
